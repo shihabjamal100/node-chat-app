7b28291f435955c245496eac7fd91d8aefa43c71
Shihab Jamal <shihab_jamal@yahoo.com>
2017-11-09 18:49:21 +1100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname + '/../public');
+const port = process.env.PORT || 3000;
+
+app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log(`Server is up on port ${port}`);
+});
