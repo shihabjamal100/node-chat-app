df7df03264ec76bde055d18f69239c89dc905e53
Shihab Jamal <shihab_jamal@yahoo.com>
2017-11-10 22:32:18 +1100

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const http = require('http');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
 
@@ -14,34 +16,15 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     // ====== socket.emit only emits the event only to this one socket.=======
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined chat',
-        createdAt: new Date().getTime()
-    });
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined chat'));
 
     socket.on('createMessage', (newMeessage) => {
         console.log('createMessage', newMeessage);
 
         // =========== io.emit emits the event to all the clients connected inluding this socket=========
-        /* io.emit('newMessage', {
-            from: newMeessage.from,
-            text: newMeessage.text,
-            createdAt: new Date().getTime()
-        }); */
-
-        // ========== socket.broadcast.emit on the other hand emits the event to all the other users except this socket========
-        /* socket.broadcast.emit('newMessage', {
-            from: newMeessage.from,
-            text: newMeessage.text,
-            createdAt: new Date().getTime()
-        }); */
+        io.emit('newMessage', generateMessage(newMeessage.from, newMeessage.text));
     });
 
     socket.on('disconnect', () => {
